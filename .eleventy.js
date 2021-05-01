8e97b0e2bfb462acad89e500a2346e32d50ff3d2
Chuck McKnight <chuckmcknight@pheonic.com>
2021-04-30 18:23:25 -0700

    Corrected addpassthroughcopy for app files

--- .eleventy.js
+++ .eleventy.js
@@ -1,79 +1,76 @@
-const moment = require("moment");
+const moment = require('moment');
 
-moment.locale("en");
+moment.locale('en');
 
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("src/js");
-    eleventyConfig.addPassthroughCopy("src/css");
-    eleventyConfig.addPassthroughCopy("src/_data");
-    eleventyConfig.addPassthroughCopy("src/img");
-    eleventyConfig.addPassthroughCopy({
-        "src/pages/topics/game-aids/tunnels-and-trolls/chargen/css": "topics/game-aids/tunnels-and-trolls/chargen/css"
-    });
+  eleventyConfig.addPassthroughCopy('src/js');
+  eleventyConfig.addPassthroughCopy('src/css');
+  eleventyConfig.addPassthroughCopy({
+    'src/pages/topics/game-aids/tunnels-and-trolls/chargen/css':
+      'topics/game-aids/tunnels-and-trolls/tunnels-and-trolls-character-generator/css',
+  });
+  eleventyConfig.addPassthroughCopy({
+    'src/pages/topics/game-aids/tunnels-and-trolls/chargen/js':
+      'topics/game-aids/tunnels-and-trolls/tunnels-and-trolls-character-generator/js',
+  });
+  eleventyConfig.addPassthroughCopy('src/_data');
+  eleventyConfig.addPassthroughCopy('src/img');
 
-    eleventyConfig.addPassthroughCopy({
-        "src/pages/topics/game-aids/tunnels-and-trolls/chargen/js": "topics/game-aids/tunnels-and-trolls/chargen/js"
-        }
-    );
-    eleventyConfig.setQuietMode(true);
-    eleventyConfig.addFilter("dateIso", (date) => {
-        return moment(date).toISOString();
-    });
+  eleventyConfig.setQuietMode(true);
+  eleventyConfig.addFilter('dateIso', (date) => {
+    return moment(date).toISOString();
+  });
 
-    eleventyConfig.addFilter("dateReadable", (date) => {
-        return moment(date).utc().format("LL");
-    });
+  eleventyConfig.addFilter('dateReadable', (date) => {
+    return moment(date).utc().format('LL');
+  });
 
-    eleventyConfig.addFilter("log", (value) => {
-        console.log(value);
-    });
+  eleventyConfig.addFilter('log', (value) => {
+    console.log(value);
+  });
 
-    eleventyConfig.addShortcode("excerpt", (article) => extractExcerpt(article));
+  eleventyConfig.addShortcode('excerpt', (article) => extractExcerpt(article));
 
-    return {
-        dir: {
-            output: "dist",
-            input: "src",
-            data: "_data",
-            includes: "partials_layouts",
-        },
-    };
-
+  return {
+    dir: {
+      output: 'dist',
+      input: 'src',
+      data: '_data',
+      includes: 'partials_layouts',
+    },
+  };
 };
 
 function extractExcerpt(article) {
-    if (!article.hasOwnProperty("templateContent")) {
-        console.warn(
-            'Failed to extract excerpt: Document has no property "templateContent".'
-        );
-        return null;
-    }
+  if (!article.hasOwnProperty('templateContent')) {
+    console.warn('Failed to extract excerpt: Document has no property "templateContent".');
+    return null;
+  }
 
-    let excerpt = null;
-    const content = article.templateContent;
+  let excerpt = null;
+  const content = article.templateContent;
 
-    // The start and end separators to try and match to extract the excerpt
-    const separatorsList = [{
-        start: "<!-- Excerpt Start -->",
-        end: "<!-- Excerpt End -->"
+  // The start and end separators to try and match to extract the excerpt
+  const separatorsList = [
+    {
+      start: '<!-- Excerpt Start -->',
+      end: '<!-- Excerpt End -->',
     },
     {
-        start: "<p>",
-        end: "</p>"
+      start: '<p>',
+      end: '</p>',
     },
-    ];
+  ];
 
-    separatorsList.some((separators) => {
-        const startPosition = content.indexOf(separators.start);
-        const endPosition = content.indexOf(separators.end);
+  separatorsList.some((separators) => {
+    const startPosition = content.indexOf(separators.start);
+    const endPosition = content.indexOf(separators.end);
 
-        if (startPosition !== -1 && endPosition !== -1) {
-            excerpt = content
-                .substring(startPosition + separators.start.length, endPosition)
-                .trim();
-            return true; //
-        }
-    });
+    if (startPosition !== -1 && endPosition !== -1) {
+      excerpt = content.substring(startPosition + separators.start.length, endPosition).trim();
+      return true; //
+    }
+  });
 
-    return excerpt;
+  return excerpt;
 }
